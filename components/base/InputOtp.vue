9d51a14b71ee223b6eab7244d32dfae70bea030d
RoyLee <royphotospace@Roys-Macbook-Air.local>
2026-01-31 17:45:25 +0800

    feat(otp) verify otp code  error success msg

--- components/base/InputOtp.vue
+++ components/base/InputOtp.vue
@@ -1,12 +1,16 @@
-<!-- 元件需要的功能
+<!-- 元件基本需要的功能
 - [ ] 可支援 4-8 位數字驗證碼（預設 6 位） // ok
 - [ ] 輸入數字或是 Tab 後自動跳到下一格 // ok
 - [ ] 按 Backspace 回到上一格 // ok
 - [ ] 任何輸入框內可一次貼上完整驗證碼  // ok
 - [ ] 只允許輸入 0-9 數字 // ok
 - [ ] 輸入完成時觸發 complete 事件 // ok
-- [ ] 支援錯誤樣式和訊息顯示
-- [ ] 透過API 驗證與處理otp驗證碼：`/api/examples/verify-otp-simple` 
+- [ ] 支援錯誤樣式和訊息顯示 // ok
+- [ ] 透過API 驗證與處理otp驗證碼：`/api/examples/verify-otp-simple`  // ok
+
+補上：
+  驗證成功的訊息顯示3秒後消失
+  驗證失敗時，清空輸入匡，並回到第一格
 -->
 <script setup lang="ts">
 import { ref, onMounted, computed } from 'vue';
@@ -39,11 +43,19 @@ const otpLength = computed(() => {
   return Math.max(4, Math.min(8, length)); // 限制在 4 - 8 碼
 });
 
-const otpData = ref<string[]>([]);
+const otpData = ref<string[]>([]); // 儲存輸入的值
 const otpInputEl = ref<HTMLInputElement[]>([]); // 儲存 input 元素DOM
 
-const isComplete = computed(() => {
+const isComplete = computed(() => { // 檢查是否輸入完成
   return otpData.value.every(item => item !== '' && validateInput(item));
+});
+
+// 驗證錯誤時清空輸入匡，並回到第一格
+watch(() => props.error, (newVal) => {
+  if(newVal) {
+    focusInputByIndex(0);
+    otpData.value = Array.from({ length: otpLength.value }, () => '');
+  }
 });
 
 // 限制輸入的值（只接受 0-9 ）
@@ -117,7 +129,11 @@ onMounted(() => {
 </script>
 <template>
   <div class="flex flex-col w-full h-dvh justify-center items-center bg-gray-200">
-    <div class="sm:flex sm:items-center sm:justify-center gap-2">
+    <div class="flex items-center justify-center gap-2 relative"
+      :class="{
+        'grid grid-cols-4 sm:flex' : otpLength == 8, // 行動裝置樣式
+      }"
+    >
       <input
         v-for="(item, index) in otpData"
         :key="index"
@@ -127,12 +143,47 @@ onMounted(() => {
         maxlength="1"
         inputmode="numeric"
         autocomplete="one-time-code"
-        class="w-20 h-20 rounded-xl border-2 border-gray-600 bg-white px-3 py-2 text-center text-2xl text-black outline-0"
+        class="relative z-50 size-12 sm:size-16 rounded-xl 
+        border-2 border-gray-600 bg-white outline-none
+        text-center text-2xl text-black
+        transition-all duration-300 ease-linear
+        focus:border-blue-400 focus:ring-2 focus:ring-blue-200"
+        :class="{
+          'border-red-500 focus:border-red-500 focus:ring-red-200': props.error,
+          'border-green-500 focus:border-green-500 focus:ring-green-200': props.success,
+        }"
         :disabled="props.disabled"
         @input="handleInput($event, index)"
         @paste="handlePaste($event, index)"
         @keydown="handleKeyDown($event, index)"
       >
+      <!-- 錯誤訊息 -->
+      <transition name="fade">
+        <div class="absolute -bottom-8 left-0 w-full" v-if="props.error">
+          <div class="text-center text-red-500 mt-2">
+            {{ props.errorMessage }}
+          </div>
+        </div>
+      </transition>
+      <!-- 成功訊息 -->
+      <transition name="fade">
+        <div class="absolute -bottom-8 left-0 w-full" v-if="props.success">
+          <div class="text-center text-green-500 mt-2">
+            {{ props.successMessage }}
+          </div>
+        </div>
+      </transition>
     </div>
   </div>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,63 @@
 <script setup lang="ts">
 
-const handleComplete = (value: string) => {
-  console.log("輸入完成: ", value);
+type otpResData = {
+  success: boolean
+  data: {
+    verified: boolean
+    timestamp: string
+  }
 }
+
+const verifyError = ref(false)
+const verifyErrorMessage = ref('')
+const verifySuccess = ref(false)
+const verifySuccessMessage = ref('')
+
+const resetStatus = () => {
+  verifyError.value = false
+  verifyErrorMessage.value = ''
+  verifySuccess.value = false
+  verifySuccessMessage.value = ''
+}
+
+const verifyOtp = async (value: string)=>{ 
+  try {
+    const res = await useHttp().post('/examples/verify-otp-simple', {
+      body: {
+        otp: value
+      }
+    }) as otpResData
+    if(res.success) {
+      verifySuccess.value = true
+      verifySuccessMessage.value = 'Verify Success'
+    } else {
+      verifyError.value = true
+      verifyErrorMessage.value = ' Verify Failed'
+    }
+
+  } catch (error) {
+    console.error(error)
+    verifyError.value = true
+    verifyErrorMessage.value = 'Please try again later'
+  } finally {
+    setTimeout(() => {
+      resetStatus()
+    }, 3000)
+  }
+}
+
+const handleComplete = (value: string) => {
+  verifyOtp(value)
+}
+
 </script>
 
 <template>
-  <BaseInputOtp :length="6" @complete="handleComplete" />
+  <BaseInputOtp 
+    @complete="handleComplete" 
+    :error="verifyError" 
+    :error-message="verifyErrorMessage" 
+    :success="verifySuccess" 
+    :success-message="verifySuccessMessage" 
+  />
 </template>
