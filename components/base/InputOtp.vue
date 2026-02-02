31b7553726da34f1308231106820cc92e9fdf207
Roy <roy@gastom.com.tw>
2026-02-02 10:42:11 +0800

    fix(otp): paste error

--- components/base/InputOtp.vue
+++ components/base/InputOtp.vue
@@ -10,13 +10,13 @@
 
 補上：
   驗證成功的訊息顯示3秒後消失
-  驗證失敗時，清空輸入匡，並回到第一格
+  驗證失敗時，清空輸入匡
   submit 按鈕？ 參考預覽圖片 assets/preview.png
 -->
 <script setup lang="ts">
-import { ref, onMounted, computed } from 'vue';
+import { ref, onMounted, computed } from 'vue'
 
-type Props = { 
+type Props = {
   length?: number;
   error?: boolean;
   errorMessage?: string;
@@ -33,107 +33,109 @@ const props = withDefaults(defineProps<Props>(), {
   disabled: false,
   success: false,
   successMessage: ''
-});
+})
 
 const emit = defineEmits<{
-  (e: 'complete', value: string): void
-}>();
+ (e: 'complete', value: string): void
+}>()
 
 const otpLength = computed(() => {
-  let length = props.length ?? 6;
-  return Math.max(4, Math.min(8, length)); // 限制在 4 - 8 碼
-});
+  const length = props.length ?? 6
+  return Math.max(4, Math.min(8, length)) // 限制在 4 - 8 碼
+})
 
-const otpData = ref<string[]>([]); // 儲存輸入的值
-const otpInputEl = ref<HTMLInputElement[]>([]); // 儲存 input 元素DOM
+const otpData = ref<string[]>([]) // 儲存輸入的值
+const otpInputEl = ref<HTMLInputElement[]>([]) // 儲存 input 元素DOM
 
 const isComplete = computed(() => { // 檢查是否輸入完成
-  return otpData.value.every(item => item !== '' && validateInput(item));
-});
+  return otpData.value.every(item => item !== '' && validateInput(item))
+})
 
 // 驗證錯誤時清空輸入匡
 watch(() => props.error, (newVal) => {
-  if(newVal) otpData.value = Array.from({ length: otpLength.value }, () => '');
-});
+  if (newVal) { otpData.value = Array.from({ length: otpLength.value }, () => '') }
+})
 
 // 限制輸入的值（只接受 0-9 ）
 const validateInput = (value: string) => {
-  return /^[0-9]$/.test(value);
+  return /^[0-9]$/.test(value)
 }
 
 // focus 指定索引的 input 元素
 const focusInputByIndex = (index: number) => {
-  otpInputEl.value[index].focus();
+  otpInputEl.value[index].focus()
 }
 
 const selectInputByIndex = (index: number) => {
-  otpInputEl.value[index].select();
+  otpInputEl.value[index].select()
 }
 
 const handleInput = (event: Event, index: number) => {
-  const inputValue = (event.target as HTMLInputElement).value;
-  const input = event.target as HTMLInputElement;
+  const inputValue = (event.target as HTMLInputElement).value
+  const input = event.target as HTMLInputElement
   if (!validateInput(inputValue)) {
-    input.value = '';
-    otpData.value[index] = '';
-    return;
+    input.value = ''
+    otpData.value[index] = ''
+    return
   }
 
-  otpData.value[index] = inputValue;
+  otpData.value[index] = inputValue
   if (index < otpLength.value - 1) {
-    focusInputByIndex(index + 1);
+    focusInputByIndex(index + 1)
   }
 
-  if(isComplete.value) {
-    emit('complete', otpData.value.join(''));
+  if (isComplete.value) {
+    emit('complete', otpData.value.join(''))
   }
 }
 
 // 處理貼上
-const handlePaste = (event: ClipboardEvent, index: number) => {
-  const clipboardData = event.clipboardData;
-  if(!clipboardData) return;
-  
-  const pastedData = clipboardData.getData('text');
-  // 字串先轉成陣列 再過濾掉非數字的值 
+const handlePaste = (event: ClipboardEvent) => {
+  event.preventDefault()
+  const clipboardData = event.clipboardData
+  if (!clipboardData) { return }
+
+  const pastedData = clipboardData.getData('text')
+  // 字串先轉成陣列 再過濾掉非數字的值
   pastedData.split('').filter(item => validateInput(item)).forEach((item, index) => {
-    otpData.value[index] = item;
-  });
+    otpData.value[index] = item
+  })
 }
 
 const handleKeyDown = (event: KeyboardEvent, index: number) => {
-  switch(event.key) {
-    case 'Backspace':  // 刪除鍵
+  switch (event.key) {
+    case 'Backspace': // 刪除鍵
       // 開啟阻止預設行為，有值先清空，沒值才退回上一格
-      // event.preventDefault(); 
-      if(otpData.value[index] !== '') {
-        otpData.value[index] = '';
-        return;
+      // event.preventDefault();
+      if (otpData.value[index] !== '') {
+        otpData.value[index] = ''
+        return
       }
-      if(index > 0) {
-        focusInputByIndex(index - 1);
-        selectInputByIndex(index - 1);
+      if (index > 0) {
+        focusInputByIndex(index - 1)
+        selectInputByIndex(index - 1)
       }
-      break;
+      break
     default:
-      break;
+      break
   }
 }
 
 const handleSubmit = () => {
-  if(props.disabled) return;
-  if(!isComplete.value) return;
-  emit('complete', otpData.value.join(''));
+  if (props.disabled) { return }
+  if (!isComplete.value) { return }
+  emit('complete', otpData.value.join(''))
 }
 
 onMounted(() => {
-  otpData.value = Array.from({ length: otpLength.value }, () => '');
-});
+  otpData.value = Array.from({ length: otpLength.value }, () => '')
+})
 
 </script>
 <template>
-  <div class="flex flex-col w-full h-dvh justify-center items-center bg-gray-200">
-    <div class="flex items-center justify-center gap-2 relative"
+  <div class="flex h-dvh w-full flex-col items-center justify-center bg-gray-200">
+    <div
+      class="relative flex items-center justify-center gap-2"
       :class="{
         'grid grid-cols-4 sm:flex' : otpLength == 8, // 行動裝置樣式
       }"
@@ -141,49 +143,56 @@ onMounted(() => {
       <input
         v-for="(item, index) in otpData"
         :key="index"
-        :value="item"
         ref="otpInputEl"
+        :value="item"
         type="text"
         maxlength="1"
         inputmode="numeric"
         autocomplete="one-time-code"
-        class="relative z-50 size-12 sm:size-16 rounded-xl 
-        border-2 border-gray-600 bg-white outline-none
-        text-center text-2xl text-black
-        transition-all duration-300 ease-linear
-        focus:border-blue-400 focus:ring-2 focus:ring-blue-200"
+        class="relative z-50 size-12 rounded-xl border-2
+        border-gray-600 bg-white text-center text-2xl
+        text-black outline-none transition-all
+        duration-300 ease-linear focus:border-blue-400
+        focus:ring-2 focus:ring-blue-200 sm:size-16"
         :class="{
           'border-red-500 focus:border-red-500 focus:ring-red-200': props.error,
           'border-green-500 focus:border-green-500 focus:ring-green-200': props.success,
         }"
         :disabled="props.disabled"
         @input="handleInput($event, index)"
-        @paste="handlePaste($event, index)"
+        @paste="handlePaste($event)"
         @keydown="handleKeyDown($event, index)"
       >
       <!-- 錯誤訊息 -->
       <transition name="fade">
-        <div class="absolute -bottom-8 left-0 w-full" v-if="props.error">
-          <div class="text-center text-red-500 mt-2">
+        <div
+          v-if="props.error"
+          class="absolute -bottom-8 left-0 w-full"
+        >
+          <div class="mt-2 text-center text-red-500">
             {{ props.errorMessage }}
           </div>
         </div>
       </transition>
       <!-- 成功訊息 -->
       <transition name="fade">
-        <div class="absolute -bottom-8 left-0 w-full" v-if="props.success">
-          <div class="text-center text-green-500 mt-2">
+        <div
+          v-if="props.success"
+          class="absolute -bottom-8 left-0 w-full"
+        >
+          <div class="mt-2 text-center text-green-500">
             {{ props.successMessage }}
           </div>
         </div>
       </transition>
     </div>
     <div>
-      <button @click="handleSubmit()"
-        class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-400 mt-12"
+      <button
+        class="mt-12 rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-400"
         :class="{
-          'bg-gray-500 hover:bg-gray-400 text-gray-300 cursor-not-allowed': props.disabled,
+          'cursor-not-allowed bg-gray-500 text-gray-300 hover:bg-gray-400': props.disabled,
         }"
+        @click="handleSubmit()"
       >
         Submit
       </button>
